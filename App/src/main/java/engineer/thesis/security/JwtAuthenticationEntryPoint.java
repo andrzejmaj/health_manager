c6922d2fa7c601d036f40e004fa3c9aadfc978c7
Kamil <kmacieslik@gmail.com>
2017-04-10 01:13:50 +0200

    AuthenticationEntryPoint created.

---
+++ App/src/main/java/engineer/thesis/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package engineer.thesis.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Created by Kamil on 2017-04-10.
+ */
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Unathorized");
+    }
+}
