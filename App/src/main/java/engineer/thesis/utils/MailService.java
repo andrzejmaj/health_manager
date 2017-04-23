7973037835d59856539be1a880093ab1c570ee4b
Andrzej Maj <andrzejmaj123@Andrzejs-MacBook-Pro.local>
2017-04-23 12:19:06 +0200

    Mail service update

--- App/src/main/java/engineer/thesis/utils/MailService.java
+++ App/src/main/java/engineer/thesis/utils/MailService.java
@@ -7,19 +7,20 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 @Component
 public class MailService {
 
-//    @Autowired
-//    private JavaMailSender mailSender;
+    @Autowired
+    private JavaMailSender mailSender;
 
     public SimpleMailMessage constructResetTokenEmail(
             String contextPath, Locale locale, String token, User user) {
-        String url = contextPath + "/changePassword?id=" +
+        String url = "http://localhost:8080/changePassword?id=" +
                 user.getId() + "&token=" + token;
-        String message = "Reseting your password";
+        String message = "Your password has been reset.\n Please click on link below to set your new password.";
         return constructEmail("Reset Password", message + " \r\n" + url, user);
     }
 
@@ -28,13 +29,13 @@ public class MailService {
         SimpleMailMessage email = new SimpleMailMessage();
         email.setSubject(subject);
         email.setText(body);
-        email.setTo(user.getEmail());
+        email.setTo(new String[]{"andrzejmaj123@gmail.com", "l.cynarski@gmail.com", "kmacieslik@gmail.com"});
         email.setFrom("sender");
         return email;
     }
 
     public void send(SimpleMailMessage message) {
         System.out.println("Sending: " + message);
-//        mailSender.send(message);
+        mailSender.send(message);
     }
 }
