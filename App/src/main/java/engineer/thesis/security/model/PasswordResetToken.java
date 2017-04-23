e027d3f69655ccbb97ca32e787ba3d5cf46d8d45
Andrzej Maj <andrzejmaj123@Andrzejs-MacBook-Pro.local>
2017-04-23 02:34:05 +0200

    [Security] Password reset token entity update

--- App/src/main/java/engineer/thesis/security/model/PasswordResetToken.java
+++ App/src/main/java/engineer/thesis/security/model/PasswordResetToken.java
@@ -1,24 +1,38 @@
 package engineer.thesis.security.model;
 
 import engineer.thesis.model.User;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Date;
 
+@Data
 @Entity
+@Table(name = "reset_tokens")
 public class PasswordResetToken {
 
-    private static final int EXPIRATION = 60 * 24;
+    private static final Long EXPIRATION = 1000L;
+
+    public PasswordResetToken() {
+    }
+
+    public PasswordResetToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+        this.expiryDate = new Date(System.currentTimeMillis() + EXPIRATION * 10000000);
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "token")
     private String token;
 
     @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "id")
+    @JoinColumn(nullable = false, name = "user_id")
     private User user;
 
+    @Column(name = "expiration_date", nullable = false)
     private Date expiryDate;
 }
