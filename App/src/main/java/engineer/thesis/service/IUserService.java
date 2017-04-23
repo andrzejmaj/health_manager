b2077f55b8074b7a8b10c92ec3ffc7dfefd68da1
Andrzej Maj <andrzejmaj123@Andrzejs-MacBook-Pro.local>
2017-04-23 02:32:23 +0200

    [Authentication] added methods to user service

--- App/src/main/java/engineer/thesis/service/IUserService.java
+++ App/src/main/java/engineer/thesis/service/IUserService.java
@@ -13,4 +13,10 @@ public interface IUserService {
     Optional<User> findByEmail(String email);
 
     Optional<User> registerNewUser(RegisterRequest registerRequest, UserRole userRole);
+
+    void createPasswordResetTokenForUser(User user, String token);
+
+    String checkReceivedToken(long id, String token);
+
+    void changeUserPassword(User user, String password);
 }
