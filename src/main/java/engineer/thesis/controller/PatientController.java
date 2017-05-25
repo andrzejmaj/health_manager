b0729e0e408e1e6c3ba56aa0e775ae5dda95aa48
andrzejmaj <andrzejmaj123@gmail.com>
2017-05-25 04:22:44 +0200

    Add missing annotation

--- src/main/java/engineer/thesis/controller/PatientController.java
+++ src/main/java/engineer/thesis/controller/PatientController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@RestController
 public class PatientController {
 
     @Autowired
