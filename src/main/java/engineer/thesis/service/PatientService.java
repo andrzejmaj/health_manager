7cdfb6f748061f7fa3677a96e936d07345f59765
Andrzej Maj <andrzejmaj123@Andrzejs-MacBook-Pro.local>
2017-05-25 02:59:38 +0200

    [HM] fix

--- src/main/java/engineer/thesis/service/PatientService.java
+++ src/main/java/engineer/thesis/service/PatientService.java
@@ -23,17 +23,19 @@ public class PatientService implements IPatientService {
     @Override
     public PatientDTO findById(Long id) throws NoSuchElementException {
         Optional<Patient> patient = Optional.ofNullable(patientRepository.findOne(id));
-        return new PatientDTO(patient.orElseThrow(() -> {
+        PatientDTO p = new PatientDTO(patient.orElseThrow(() -> {
             throw new NoSuchElementException("Patient not found");
         }));
+         return  p;
     }
 
     @Override
     public PatientDTO findByPesel(String pesel) throws NoSuchElementException {
         Optional<Patient> patient = Optional.ofNullable(patientRepository.findPatientByPesel(pesel));
-        return new PatientDTO(patient.orElseThrow(() -> {
+        PatientDTO p =  new PatientDTO(patient.orElseThrow(() -> {
             throw new NoSuchElementException("Patient not found");
         }));
+        return  p;
     }
 
 }
