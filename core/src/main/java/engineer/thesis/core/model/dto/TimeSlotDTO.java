37d3f9e24df98ae371eeefbce6f4e85be3741856
Filip Galas <fgalas525@gmail.com>
2017-07-25 16:46:49 +0200

    Cleanup TimeSlot model

--- core/src/main/java/engineer/thesis/core/model/dto/TimeSlotDTO.java
+++ core/src/main/java/engineer/thesis/core/model/dto/TimeSlotDTO.java
@@ -2,13 +2,12 @@ package engineer.thesis.core.model.dto;
 
 import lombok.Value;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Value
 public class TimeSlotDTO {
     Long id;
     Long doctorId;
-    Date startDateTime;
-    Date endDateTime;
-    Long TimeSlotTypeId;
+    LocalDateTime startDateTime;
+    LocalDateTime endDateTime;
 }
