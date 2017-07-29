43cd0508a2d1045b6a8c7f084430c949ea7b9a88
Wojciech Karpiel <w.karpiel@yandex.com>
2017-07-29 09:34:29 +0200

    [PRZEMEK-6] Poprawki do zapisywania się na wizyty

--- core/src/main/java/engineer/thesis/core/controller/AppointmentController.java
+++ core/src/main/java/engineer/thesis/core/controller/AppointmentController.java
@@ -1,17 +1,13 @@
 package engineer.thesis.core.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import engineer.thesis.core.model.dto.AppointmentDTO;
 import engineer.thesis.core.repository.AppointmentRepository;
 import engineer.thesis.core.repository.PatientRepository;
 import engineer.thesis.core.service.IAppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppointmentController {
@@ -32,11 +28,11 @@ public class AppointmentController {
 		return new ResponseEntity<>(appointmentRepository.findByPatientId(patientId), HttpStatus.OK);
 	}
 
-	@RequestMapping(method = RequestMethod.PUT, path = "/patients/{patientId}/appointments/")
-	public ResponseEntity<?> createAppointment(@PathVariable(name = "patientId") long patientId,
-			AppointmentDTO appointmentDTO) {
-		if (!patientRepository.exists(patientId)) {
-			return new ResponseEntity<>("Patient with id " + patientId + "not found", HttpStatus.NOT_FOUND);
+    @RequestMapping(method = RequestMethod.PUT, path = "/patients/{patientId}/appointments")
+    public ResponseEntity<?> createAppointment(@PathVariable(name = "patientId") long patientId,
+                                               @RequestBody AppointmentDTO appointmentDTO) {
+        if (!patientRepository.exists(patientId)) {
+            return new ResponseEntity<>("Patient with id " + patientId + "not found", HttpStatus.NOT_FOUND);
 		}
 
 		if (appointmentService.exists(appointmentDTO)) {
