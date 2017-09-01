4bc2e57dec3d663636e0d1c1c912d4a7af2351e9
Herioz <kmacieslik@gmail.com>
2017-09-02 01:40:49 +0200

    Included missing exception.

---
+++ core/src/main/java/engineer/thesis/core/exception/NoSuchElementExistsException.java
@@ -0,0 +1,7 @@
+package engineer.thesis.core.exception;
+
+public class NoSuchElementExistsException extends Exception {
+    public NoSuchElementExistsException(String message) {
+        super(message);
+    }
+}
