60c1b803e095311cd2ac3f86ebfaedee64c06cc9
Andrzej Maj <andrzejmaj123@Andrzejs-MacBook-Pro.local>
2017-09-01 21:21:10 +0200

    Quick validation fix

--- core/src/main/java/engineer/thesis/core/service/Implementation/MedicalCheckupService.java
+++ core/src/main/java/engineer/thesis/core/service/Implementation/MedicalCheckupService.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class MedicalCheckupService implements IMedicalCheckupService {
+public class MedicalCheckupService {
 
     @Autowired
     private MedicalCheckupRepository medicalCheckupRepository;
@@ -25,51 +25,51 @@ public class MedicalCheckupService implements IMedicalCheckupService {
     @Autowired
     private CustomObjectMapper objectMapper;
 
-    @Override
-    public List<MedicalCheckupDTO> getPatientCheckups(Long id) throws NoSuchElementExistsException {
-        if (!patientRepository.exists(id)) {
-            throw new NoSuchElementExistsException("Patient doesn't exists");
-        }
-        return medicalCheckupRepository.findAllByPatientId_Id(id).stream().map(medicalCheckup -> objectMapper.convert(medicalCheckup, MedicalCheckupDTO.class)).collect(Collectors.toList());
-    }
-
-    @Override
-    public MedicalCheckupDTO saveMedicalCheckup(Long patientId, MedicalCheckupDTO medicalCheckupDTO) throws NoSuchElementExistsException, DataIntegrityException {
-        if (!patientId.equals(medicalCheckupDTO.getPatientId())) {
-            throw new DataIntegrityException("Ids in path and body does not match");
-        }
-        if (!patientRepository.exists(medicalCheckupDTO.getPatientId())) {
-            throw new NoSuchElementExistsException("Patient doesn't exist");
-        }
-        medicalCheckupDTO.setId(null);
-        return objectMapper.convert(medicalCheckupRepository.save(
-                objectMapper.convert(medicalCheckupDTO, MedicalCheckup.class)), MedicalCheckupDTO.class);
-    }
-
-    @Override
-    public MedicalCheckupDTO updateMedicalCheckup(Long patientId, MedicalCheckupDTO medicalCheckupDTO) throws NoSuchElementExistsException, DataIntegrityException {
-
-        if (!patientId.equals(medicalCheckupDTO.getPatientId())) {
-            throw new DataIntegrityException("Ids in path and body does not match");
-        }
-
-        if (!patientRepository.exists(medicalCheckupDTO.getPatientId())) {
-            throw new NoSuchElementExistsException("Patient doesn't exist");
-        }
-
-        if (medicalCheckupRepository.exists(medicalCheckupDTO.getId())) {
-            throw new NoSuchElementExistsException("Medical checkup doesn't exist");
-        }
-
-        return objectMapper.convert(medicalCheckupRepository.save(
-                objectMapper.convert(medicalCheckupDTO, MedicalCheckup.class)), MedicalCheckupDTO.class);
-    }
-
-    @Override
-    public void delete(Long id) throws NoSuchElementExistsException {
-        if (!medicalCheckupRepository.exists(id)) {
-            throw new NoSuchElementExistsException("Checkup doesn't exists");
-        }
-        medicalCheckupRepository.delete(id);
-    }
+//    @Override
+//    public List<MedicalCheckupDTO> getPatientCheckups(Long id) throws NoSuchElementExistsException {
+//        if (!patientRepository.exists(id)) {
+//            throw new NoSuchElementExistsException("Patient doesn't exists");
+//        }
+//        return medicalCheckupRepository.findAllByPatientId_Id(id).stream().map(medicalCheckup -> objectMapper.convert(medicalCheckup, MedicalCheckupDTO.class)).collect(Collectors.toList());
+//    }
+//
+//    @Override
+//    public MedicalCheckupDTO saveMedicalCheckup(Long patientId, MedicalCheckupDTO medicalCheckupDTO) throws NoSuchElementExistsException, DataIntegrityException {
+//        if (!patientId.equals(medicalCheckupDTO.getPatientId())) {
+//            throw new DataIntegrityException("Ids in path and body does not match");
+//        }
+//        if (!patientRepository.exists(medicalCheckupDTO.getPatientId())) {
+//            throw new NoSuchElementExistsException("Patient doesn't exist");
+//        }
+//        medicalCheckupDTO.setId(null);
+//        return objectMapper.convert(medicalCheckupRepository.save(
+//                objectMapper.convert(medicalCheckupDTO, MedicalCheckup.class)), MedicalCheckupDTO.class);
+//    }
+//
+//    @Override
+//    public MedicalCheckupDTO updateMedicalCheckup(Long patientId, MedicalCheckupDTO medicalCheckupDTO) throws NoSuchElementExistsException, DataIntegrityException {
+//
+//        if (!patientId.equals(medicalCheckupDTO.getPatientId())) {
+//            throw new DataIntegrityException("Ids in path and body does not match");
+//        }
+//
+//        if (!patientRepository.exists(medicalCheckupDTO.getPatientId())) {
+//            throw new NoSuchElementExistsException("Patient doesn't exist");
+//        }
+//
+//        if (medicalCheckupRepository.exists(medicalCheckupDTO.getId())) {
+//            throw new NoSuchElementExistsException("Medical checkup doesn't exist");
+//        }
+//
+//        return objectMapper.convert(medicalCheckupRepository.save(
+//                objectMapper.convert(medicalCheckupDTO, MedicalCheckup.class)), MedicalCheckupDTO.class);
+//    }
+//
+//    @Override
+//    public void delete(Long id) throws NoSuchElementExistsException {
+//        if (!medicalCheckupRepository.exists(id)) {
+//            throw new NoSuchElementExistsException("Checkup doesn't exists");
+//        }
+//        medicalCheckupRepository.delete(id);
+//    }
 }
