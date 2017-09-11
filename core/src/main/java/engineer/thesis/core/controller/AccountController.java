2a15b6689fe075be70d97e089592a6924b443ead
Herioz <kmacieslik@gmail.com>
2017-09-11 13:18:04 +0200

    Removed unnecessary try/catch.

--- core/src/main/java/engineer/thesis/core/controller/AccountController.java
+++ core/src/main/java/engineer/thesis/core/controller/AccountController.java
@@ -82,11 +82,7 @@ public class AccountController {
 
     @RequestMapping(path = RequestMappings.ACCOUNTS.ACCOUNTS_ROLE, method = RequestMethod.GET)
     public ResponseEntity<?> getMyRole() {
-        try {
-            return new ResponseEntity<>(accountService.getMyRole(), HttpStatus.OK);
-        } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+        return new ResponseEntity<>(accountService.getMyRole(), HttpStatus.OK);
     }
 
     private SecurityUser getCurrentUser() {
