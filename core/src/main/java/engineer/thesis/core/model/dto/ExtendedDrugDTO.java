f9a8301eab7831e602158aa4575164c953aa0246
Andrzej Maj <andrzejmaj123@Andrzejs-MacBook-Pro.local>
2017-10-11 11:11:37 +0200

    Add packs list to dto

--- core/src/main/java/engineer/thesis/core/model/dto/ExtendedDrugDTO.java
+++ core/src/main/java/engineer/thesis/core/model/dto/ExtendedDrugDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,4 +17,5 @@ public class ExtendedDrugDTO {
     private String permitValidity;
     private String entityResponsible;
     private String drugForm;
+    private List<PackDTO> packs;
 }
